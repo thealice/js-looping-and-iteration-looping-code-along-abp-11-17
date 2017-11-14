8d3c10c59a03ac1c5f413894ff380744b8da1966
Alice Freda <alicefreda@gmail.com>
2017-11-14 00:17:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+const badges = ['Ada', 'Brendan', 'Ali'];
+function printBadges(badgeNumber) {
+  for (let i = 1; i < badges.length; i++;) {
+    console.log(`Welcome ${badges[i]}`)
+  }
+}
