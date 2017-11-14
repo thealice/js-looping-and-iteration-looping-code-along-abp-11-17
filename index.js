4ab1df38c967799668c2c651d575e6643affe832
Alice Freda <alicefreda@gmail.com>
2017-11-14 00:47:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges(arrayNames) {
+  for (let i = 0; i < arrayNames.length; i++) {
+    console.log(`Welcome ${arrayNames[i]}! You are employee #${i+1}.`);
+  }
+  return arrayNames;
+}
+
+function tailsUp() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let counter = 0;
+  while (tailsUp()) {
+    counter++;
+  }
+  return `You got ${counter} tails in a row!`;
+}
