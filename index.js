870a45e34e96b64ff92d81893a7e1e2242f732ee
Alice Freda <alicefreda@gmail.com>
2017-11-14 00:29:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solutions in this file
 function printBadges(arrayNames) {
-  arrayNames = ['Ada', 'Brendan', 'Ali'];
-  for (let i = 0; i < arrayNames.length; i++;) {
-    console.log(`Welcome ${arrayNames[i]}! You are employee #${[i] + 1}.`);
+  for (let i = 0; i < arrayNames.length; i++) {
+    console.log(`Welcome ${arrayNames[i]}! You are employee #${i+1}.`);
   }
 }
