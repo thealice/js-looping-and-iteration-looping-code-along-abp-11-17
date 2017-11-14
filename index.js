7f6430cecd3179de8156b8643d228ac63e36b225
Alice Freda <alicefreda@gmail.com>
2017-11-14 00:20:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 const badges = ['Ada', 'Brendan', 'Ali'];
-function printBadges(badges) {
+function printBadges(array) {
   for (let i = 1; i < badges.length; i++;) {
-    console.log(`Welcome ${badges[i]}! You are employee #[i].`);
+    console.log(`Welcome ${array[i]}! You are employee #[i].`);
   }
 }
