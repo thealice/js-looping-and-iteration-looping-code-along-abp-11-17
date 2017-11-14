b2b1405b6d701505f476357fcc0f54140cd6bf98
Alice Freda <alicefreda@gmail.com>
2017-11-14 00:37:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function printBadges(arrayNames) {
   }
   return arrayNames;
 }
+
+function tailsNeverFails() {
+  return Math.random() >= 0.5;
+}
+
+while (counter = 0; tailsNeverFails() === true; counter++) {
+  return `"You got ${counter + 1} tails in a row!"`;
+}
